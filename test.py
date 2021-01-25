2792b06a0da781314a526340b9a07c255ca14f3b
zy <zhangyao@ewell.cc>
2021-01-25 14:26:58 +0800

    modify train test detect

--- test.py
+++ test.py
@@ -302,7 +302,7 @@ if __name__ == '__main__':
     opt.save_json |= opt.data.endswith('coco.yaml')
     opt.data = check_file(opt.data)  # check file
     print(opt)
-    check_requirements()
+    # check_requirements()
 
     if opt.task in ['val', 'test']:  # run normally
         test(opt.data,
